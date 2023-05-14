eb5ac425fd765fa54c9a3e9277bbe72c759329a0
pajacob44@gmail.com <pajacob44@gmail.com>
2023-05-14 20:17:12 +0200

    yml fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mcmeddon.com',
-    //site: 'https://mcmeddon.github.io',
-    //base: '/mcmeddon.com',
 });
+
+//site: 'https://mcmeddon.github.io',
+//base: '/mcmeddon.com',
