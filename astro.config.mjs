77dc41bede6f2163f45f0af0e81c3e93cc7639af
pajacob44@gmail.com <pajacob44@gmail.com>
2023-05-14 18:16:32 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://mcmeddon.com',
+    //site: 'https://mcmeddon.github.io',
+    //base: '/mcmeddon.com',
+});
