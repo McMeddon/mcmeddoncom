624f75faeccccd347b04d2f9cebb54f298fe2f22
pajacob44@gmail.com <pajacob44@gmail.com>
2023-05-15 17:24:18 +0200

    new remote commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mcmeddon.github.io',
-    base: '/mcmeddon.com',
+    site: 'https://mcmeddon.com',
 });
 
-//site: 'https://mcmeddon.com',
+//site: 'https://mcmeddon.github.io',
+//base: '/mcmeddon.com',
