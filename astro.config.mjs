d7af16858ed1e6faa7485f7d1c1aee9391f5c229
pajacob44@gmail.com <pajacob44@gmail.com>
2023-05-15 22:38:23 +0200

    img-picture additon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mcmeddon.com',
+  site: 'https://mcmeddon.com',
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
 
 //site: 'https://mcmeddon.github.io',
